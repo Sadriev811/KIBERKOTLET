d6b49868d71f860ee7fdcc62b1f1a37bc503d3f3
SHAXA <shaxa@MacBook-Pro-SHAXA.local>
2023-04-12 14:55:52 +0500

    shaxa2 commit

--- shaxa_html/js/main.js
+++ shaxa_html/js/main.js
@@ -4,3 +4,27 @@ let menu = document.querySelector('.menu_1')
 btnSpan.addEventListener('click', function(){
     menu.classList.toggle('active')
 })
+
+// Scroll top
+
+const goTopBtn = document.querySelector(".scrollTop")
+
+goTopBtn.addEventListener("click", goTop);
+
+
+function goTop() {
+    if (window.pageXOffset > 0) {
+        window.scrollBy(0, -75);
+        setTimeout(goTop, 0);
+    }
+};
+
+const scrollBtn = document.querySelector(".isShowBtn");
+
+window.onscroll = () => {
+    if (window.scrollBy > 700) {
+        scrollBtn.classList.remove('isShowBtn_hide');
+    } else if (window.scrollBy < 700) {
+        scrollBtn.classList.add('isShowBtn_hide');
+    }
+};
